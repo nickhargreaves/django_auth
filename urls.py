f015666ce6d474bd1f0df6abb0ffe81096c26cb8
Nick Hargreaves <hargreaves1234@gmail.com>
2015-10-03 13:51:42 +0300

    login url + view

--- urls.py
+++ urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^login/$', views.login)
+    url(r'^login/$', views.login),
+    url(r'^login/$', views.login),
 ]
--- views.py
+++ views.py
@@ -1,6 +1,14 @@
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect
+from django.contrib import auth
+from django.core.context_processors import csrf
 
 def index(request):
     return HttpResponse("Not allowed here!")
+
+#Login function
 def login(request):
-    return HttpResponse("Login screen")
+    c = {}
+    c.update(csrf(request))
+    return render_to_response('login.html', c)
