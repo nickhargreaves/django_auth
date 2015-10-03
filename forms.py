72f93b5c40b4cf7c40736fd0ce99dffc60fd3de9
Nick Hargreaves <hargreaves1234@gmail.com>
2015-10-03 16:54:46 +0300

    add UserProfile model

--- forms.py
+++ forms.py
@@ -6,6 +6,7 @@ from django.contrib.auth.forms import UserCreationForm
 class CustomRegistrationForm(UserCreationForm):
     email = forms.EmailField(required=True)
 
+
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
--- models.py
+++ models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# Create a user model
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    activation_key = models.CharField(maxlength=40)
+    key_expires = models.DateTimeField()
