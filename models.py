aee27f0fd059fbdd072ce4c563260abdfdfbc03f
Nick Hargreaves <hargreaves1234@gmail.com>
2015-10-04 12:35:08 +0300

    they changed maxlength

--- models.py
+++ models.py
@@ -6,5 +6,5 @@ from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
-    activation_key = models.CharField(maxlength=40)
+    activation_key = models.CharField(max_length=40)
     key_expires = models.DateTimeField()
